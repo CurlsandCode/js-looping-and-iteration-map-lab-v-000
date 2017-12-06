184a8252948619e0c0325b59824441da9d68c352
Monika Williams <monika.m.williams@gmail.com>
2017-12-06 10:29:47 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ function lowerCaseDrivers (array){
   return array.map(function(element)) {return element.toLowerCase();}
 }
 function nameToAttributes (array){
-  return array.map(function(element))
+  return array.map(function(element)){
+    let name = "".split
+    return name[0], name[1];
+  }
 }
 function attributesToPhrase (array){
   return array.map(function(element)) {
     return "${element} is from ${element}";
   }
-  
+
 }
