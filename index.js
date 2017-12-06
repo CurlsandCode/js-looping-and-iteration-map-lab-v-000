7b83451ba256010cab94f094eb2038ddca6178c9
Monika Williams <monika.m.williams@gmail.com>
2017-12-06 10:14:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Code your solution in this file.
+function lowerCaseDrivers (drivers) {
+  const lcDrivers = [];
+  for (const element of drivers){
+    lcDrivers.push(callback(element)).toLowercase();
+  }
+}
+return lcDrivers;
