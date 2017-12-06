b5870dc9ec244729883a909251440d9150d54a6f
Monika Williams <monika.m.williams@gmail.com>
2017-12-06 10:33:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // Code your solution in this file.
-function lowerCaseDrivers (array){
-  return array.map(function(element) {return element.toLowerCase();)
-  }
-}
+function lowerCaseDrivers (array) {
+  return array.map(function (element) { return element.toLowerCase();})
+};
+
 function nameToAttributes (array){
   return array.map(function(element) {
     let name = element.split(" ")
